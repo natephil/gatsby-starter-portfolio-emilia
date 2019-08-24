2e3d6cf0d3bcad8a8e3508668a317a1116a834bc
Nathan Cahn <nathancahn@Nathans-MBP-2.ucsf.edu>
2019-08-24 02:52:25 -0700

    added ben cahn logo

--- config/site.js
+++ config/site.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Emilia', // Navigation and Site Title
-  siteTitleAlt: 'Emilia - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Emilia', // short_name for manifest
+  siteTitle: 'Ben Cahn', // Navigation and Site Title
+  siteTitleAlt: 'Gatsby Starter Portfolio Test', // Alternative Site title for SEO
+  siteTitleShort: 'Ben', // short_name for manifest
   siteHeadline: 'Publishing & Creating stunning photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://emilia.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://bencahn.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription: 'Dark One-Page portfolio with cards & detailed project views',
   author: 'LekoArts', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@emilia', // Twitter Username
+  userTwitter: '@BarackObama', // Twitter Username
   ogSiteName: 'emilia', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
   googleAnalyticsID: 'UA-47519312-4',
@@ -22,20 +22,20 @@ module.exports = {
   backgroundColor: '#2b2e3c',
 
   // Your information
-  avatar: '/logos/ben_cahn_logo.png',
+  avatar: '/logos/ben_cahn_logo_small.png',
   name: 'Ben Cahn',
   location: 'Berkeley, CA',
   socialMedia: [
     {
-      url: 'https://twitter.com/lekoarts_de',
+      url: 'https://twitter.com',
       name: 'Twitter',
     },
     {
-      url: 'https://www.instagram.com/lekoarts.de',
+      url: 'https://www.instagram.com/bendreamin/?hl=en',
       name: 'Instagram',
     },
     {
-      url: 'https://www.lekoarts.de/en',
+      url: 'https://www.bencahn.com',
       name: 'Homepage',
     },
   ],
