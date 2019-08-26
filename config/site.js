cb26ab8e47eb01ece2a0051c77ac20d8a34f88e8
Nathan Cahn <nathancahn@Nathans-MBP-2.ucsf.edu>
2019-08-26 00:40:11 -0700

    changed title

--- config/site.js
+++ config/site.js
@@ -2,12 +2,12 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'Ben Cahn', // Navigation and Site Title
-  siteTitleAlt: 'Gatsby Starter Portfolio Test', // Alternative Site title for SEO
+  siteTitleAlt: 'Ben Cahn Photography', // Alternative Site title for SEO
   siteTitleShort: 'Ben', // short_name for manifest
   siteHeadline: 'Publishing & Creating stunning photos', // Headline for schema.org JSONLD
   siteUrl: 'https://bencahn.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/src/favicon.png', // Used for SEO and manifest
+  siteLogo: '/src/favicon_camera.png', // Used for SEO and manifest
   siteDescription: 'Dark One-Page portfolio with cards & detailed project views',
   author: 'LekoArts', // Author for schema.org JSONLD
 
